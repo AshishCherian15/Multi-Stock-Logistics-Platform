202f7e2e567777fd4b43206bb4ee5dfcb3bc2ddb
Ashish Cherian <ashishcherian15@gmail.com>
2026-01-11 22:39:15 +0530

    Fix Django handler for Vercel

--- greaterwms/wsgi.py
+++ greaterwms/wsgi.py
@@ -1,8 +1,15 @@
 import os
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'greaterwms.settings')
+# Ensure project root is on Python path
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "greaterwms.settings")
+
 application = get_wsgi_application()
 
-# Required for Vercel
+# Vercel requires this
 app = application
+handler = application
