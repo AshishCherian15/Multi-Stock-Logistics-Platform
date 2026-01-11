4f9ac154ab8ed519ece08fe86700f64eea562350
Ashish Cherian <ashishcherian15@gmail.com>
2026-01-11 22:44:52 +0530

    Fix Vercel Django serverless entry

--- greaterwms/wsgi.py
+++ greaterwms/wsgi.py
@@ -1,15 +1,6 @@
 import os
-import sys
 from django.core.wsgi import get_wsgi_application
-
-# Ensure project root is on Python path
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(BASE_DIR)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "greaterwms.settings")
 
 application = get_wsgi_application()
-
-# Vercel requires this
-app = application
-handler = application
