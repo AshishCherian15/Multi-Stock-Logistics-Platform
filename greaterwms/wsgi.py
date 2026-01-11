6ecab0b5a8039b0e09316b9372ca3bf36827e6eb
Ashish Cherian <ashishcherian15@gmail.com>
2026-01-11 22:35:21 +0530

    Fix Vercel WSGI handler and collect static files

--- greaterwms/wsgi.py
+++ greaterwms/wsgi.py
@@ -3,3 +3,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'greaterwms.settings')
 application = get_wsgi_application()
+
+# Required for Vercel
+app = application
